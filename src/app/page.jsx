ed49a6763443e139c7a7a6a61caabefda74c312d
IgnacioNMiranda <i.miranda.f99@gmail.com>
2024-10-03 21:45:42 -0300

    chore: foundations

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>PDP</main>
+      <footer className={styles.footer}></footer>
+    </div>
+  )
+}
