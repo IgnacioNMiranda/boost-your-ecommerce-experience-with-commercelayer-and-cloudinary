bd0cacaa21f141803d78632c456192d4608a4d84
IgnacioNMiranda <i.miranda.f99@gmail.com>
2024-10-18 23:12:05 -0300

    docs: update README and add small homepage

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      <p>
+        Visit <code>/product/some-product-slug</code> to see the product details page of your product!
+      </p>
+    </div>
+  )
+}
